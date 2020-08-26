6d3bb53754f4bb8e4f25cd4f50376c605333822c
Nadia Ahmed <nadiaalshezawy@gmail.com>
2020-08-26 15:03:31 +0400

    working on saving new question

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,7 +1,31 @@
-import { saveQuestionAnswer } from "../utils/api";
+import { saveQuestionAnswer, saveQuestion } from "../utils/api";
+import { showLoading, hideLoading } from "react-redux-loading";
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
 export const SAVE_QUESTION_ANSWER = "SAVE_QUESTION_ANSWER";
+export const ADD_QUESTION = "ADD_QUESTION";
 
+function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  };
+}
+
+export function handleAddQuestion(optionOneText, optionTwoText) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+
+    dispatch(showLoading());
+
+    return saveQuestion({
+      optionOneText,
+      optionTwoText,
+      author: authedUser,
+    })
+      .then((question) => dispatch(addQuestion(question)))
+      .then(() => dispatch(hideLoading));
+  };
+}
 export function receiveQuestions(questions) {
   return {
     type: RECEIVE_QUESTIONS,
