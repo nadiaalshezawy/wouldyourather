fc040ee36b14baf56746fb14f4a2f7defe41247f
Nadia Ahmed <nadiaalshezawy@gmail.com>
2020-08-30 18:19:12 +0400

    add user list to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { Dropdown, Segment, Header } from "semantic-ui-react";
+import { connect } from "react-redux";
 
-const Login = () => {
-  return <div>Login</div>;
+class Login extends React.Component {
+  state = {
+    value: "",
+  };
+  handleChange = (e, { value }) => this.setState({ value });
+  render() {
+    const { users, authedUser } = this.props;
+    const { value } = this.state;
+    console.log(users);
+
+    return (
+      <Segment padded textAlign="center" size="large">
+        <Header as="h2">
+          Welacome to Would you rather application
+          <Header.Subheader>please Login</Header.Subheader>
+        </Header>
+        <select className="ui search dropdown">
+          <option value="">Select user</option>
+          {users.map((user) => (
+            <option value={user.id} key={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </Segment>
+    );
+  }
+}
+
+const mapStateToProps = ({ authedUser, users }) => {
+  // console.log(state);
+  // const question = questions[ownProps.match.params.question_id];
+
+  // console.log(question);
+  return {
+    //   question: question ? question : null,
+    users: Object.values(users),
+    authedUser,
+  };
 };
 
-export default Login;
+export default connect(mapStateToProps)(Login);
